f7504d73c95c56da0f4e27de24457f27ad4fe591
Arthurisme <is@live.ca>
2015-08-16 22:28:46 -0400

    v4.3

--- official_polls/urls.py
+++ official_polls/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from polls import views
 
 urlpatterns = [
+
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
